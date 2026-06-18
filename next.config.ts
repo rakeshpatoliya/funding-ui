ac1b53de69a35790be250a1934a0eec853987ea1
rakeshpatoliya <rakeshpatelnicsm@gmail.com>
2026-06-18 11:01:32 +0530

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    FOUNDRY_BASE_URL: process.env.FOUNDRY_BASE_URL || "",
-    ONTOLOGY_RID: process.env.ONTOLOGY_RID || "",
-    CLIENT_ID: process.env.CLIENT_ID || "",
-    DEFAULT_LOAN_NUMBER: process.env.DEFAULT_LOAN_NUMBER || "",
-  }
 };
 
 export default nextConfig;
